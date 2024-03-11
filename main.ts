35295ff34cc37e76f3c2903c3348b551b5619a7c
henriquebussi <henriquebussidossantos@gmail.com>
2024-03-11 15:29:22 -0300

    avl

---
+++ main.ts
@@ -0,0 +1,14 @@
+
+var prompt = require('prompt-sync')();
+
+console.log('ola mundo');
+
+while (true){
+var a = Number(prompt('Manda um numero ai: '))
+
+if (a == 666){
+    console.log(a, " Isso sim é um numero bom")
+} else{
+    console.log("numero cringe")    
+}
+}
